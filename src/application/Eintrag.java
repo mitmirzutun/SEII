e5de210ab3803162d626e5cddbaf0b6d0a33759e
mitmirzutun <milan1111@gmx.de>
2020-03-24 18:13:06 +0100

    Update

---
+++ src/application/Eintrag.java
@@ -0,0 +1,116 @@
+package application;
+
+public class Eintrag {
+	private String vorname,nachname,adresse,debugPrefix="application.Eintrag";
+	public static boolean DEBUGGING=false;
+	public Eintrag(String vorname, String nachname, String adresse) {
+		if (DEBUGGING) {
+			System.out.println(debugPrefix+": Call of Constructor(String, String, String)");
+		}
+		this.vorname=vorname;
+		this.nachname=nachname;
+		this.adresse=adresse;
+		if (DEBUGGING) {
+			System.out.println(debugPrefix+": First name: "+this.vorname);
+			System.out.println(debugPrefix+": Last name: "+this.nachname);
+			System.out.println(debugPrefix+": Address: "+this.adresse);
+		}
+	}
+	public Eintrag(Eintrag e) {
+		if (DEBUGGING) {
+			System.out.println(debugPrefix+": Call of Constructor(Eintrag)");
+		}
+		this.vorname=e.vorname;
+		this.nachname=e.nachname;
+		this.adresse=e.adresse;
+		if (DEBUGGING) {
+			System.out.println(debugPrefix+": First name: "+this.vorname);
+			System.out.println(debugPrefix+": Last name: "+this.nachname);
+			System.out.println(debugPrefix+": Address: "+this.adresse);
+		}
+	}
+	public String toString() {
+		StringBuilder sb = new StringBuilder(2+vorname.length()+nachname.length()+adresse.length());
+		sb.append(vorname);
+		sb.append(' ');
+		sb.append(nachname);
+		sb.append('\n');
+		sb.append(adresse);
+		return sb.toString();
+	}
+	public String getFirstName() {
+		return vorname;
+	}
+	public String getLastName() {
+		return nachname;
+	}
+	public String getAdress() {
+		return adresse;
+	}
+	public Eintrag clone() {
+		return new Eintrag(this);
+	}
+	public static boolean test() {
+		if (DEBUGGING)
+			System.out.println("Testing getFirstName");
+		Eintrag e = new Eintrag("Max","Mustermann","Musterstraﬂe 4");
+		if (!"Max".equals(e.getFirstName())) {
+			return false;
+		}
+		if (DEBUGGING)
+			System.out.println("Testing getLastName");
+		if (!"Mustermann".equals(e.getLastName())) {
+			return false;
+		}
+		if (DEBUGGING)
+			System.out.println("Testing getAdress");
+		if (!"Musterstaﬂe 4".equals(e.getAdress())) {
+			return false;
+		}
+		if (DEBUGGING)
+			System.out.println("Testing Eintrag(Eintrag)");
+		Eintrag e1 = new Eintrag(e);
+		if (!e.getAdress().equals(e1.getAdress())) {
+			return false;
+		}
+		if (!e.getFirstName().equals(e.getLastName())) {
+			return false;
+		}
+		if (!e.getLastName().equals(e.getLastName())) {
+			return false;
+		}
+		if (!e.equals(e1)) {
+			return false;
+		}
+		if (DEBUGGING)
+			System.out.println("Testing clone/equals");
+		if (!e.equals(e.clone())) {
+			return false;
+		}
+		if (e.equals(new Object()))
+			return false;
+		if (e.equals(new Eintrag("Mustermann","Max","Musterstraﬂe")))
+			return false;
+		try {
+			e.equals(null);
+		} catch (Exception exc)  {
+			if (!(exc instanceof NullPointerException))
+				return false;
+			if (!exc.getMessage().equals(new NullPointerException("No Object to compare to")))
+				return false;
+		}
+		if (DEBUGGING)
+			System.out.println("Testing toString");
+		if (!e.toString().equals("Max Mustermann\nMuserstraﬂe 4")) {
+			return false;
+		}
+		return true;
+	}
+	public boolean equals(Object o) {
+		if (o==null)
+			throw new NullPointerException("No Object to compare to");
+		if (!(o instanceof Eintrag))
+			return false;
+		return o.toString().equals(this.toString());
+	}
+}
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,19 +1,55 @@
 package application;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
+		public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			StackPane root = new StackPane();
+			TextField tf1 = new TextField();
+			tf1.setPrefColumnCount(5);
+			tf1.setMaxWidth(100);
+			TextField tf2 = new TextField();
+			tf2.setPrefColumnCount(5);
+			tf2.setMaxWidth(100);
+			Label l = new Label();
+	        // action event 
+	        EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
+	        	String[] StringArray=new String[0];
+	            public void handle(ActionEvent e) 
+	            {
+	            	String[] tmp = StringArray;
+	            	StringArray=new String[tmp.length+1];
+	            	for (int i=0;i<tmp.length;i++)
+	            		StringArray[i]=tmp[i];
+	            	StringArray[tmp.length]=tf1.getText();
+	                System.out.println(StringArray[tmp.length]);
+	                l.setText(concat(StringArray));
+	            } 
+	            private String concat(String s[]) {
+	            	int length=s.length;
+	            	for (int i=0;i<s.length;i++)
+	            		length+=s[i].length();
+	            	StringBuilder sb=new StringBuilder(length);
+	            	for (int i=0;i<s.length;i++) {
+	            		sb.append(s[i]);
+	            		sb.append('\n');
+	            	}
+	            	return sb.toString();
+	            }
+	        };
+	        tf1.setOnAction(event);
+	        tf2.setOnAction(event);
+			root.getChildren().add(tf1);
+			root.getChildren().add(l);
 			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("Hello World");
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -21,7 +57,6 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
-	
 	public static void main(String[] args) {
 		launch(args);
 	}
